53a906f0d6ce713a0767ebe010006d3c4e6fbb2a
Artemy-sweden <ghosteagle2020@mail.ru>
2024-10-07 14:05:31 +0300

    add: /constants for sizes and colors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  red: '#B51332',
+  red_shadow: '#8E1028',
+  aquamarine: '#13B57B',
+  white: '#ffffff',
+  grey_light: '#d9d9d9',
+  grey_mid: '#aeaeae',
+  grey_dark: '#5f5f5f',
+  grey_black: '#393939',
+  black: '#000000',
+  dark_black: '#222222',
+  black_black: '#1e1e1e',
+};
---
+++ src/constants/TextSizes.ts
@@ -0,0 +1,6 @@
+export const TextSize = {
+  large: 48,
+  medium: 32,
+  regular: 24,
+  small: 18,
+};
