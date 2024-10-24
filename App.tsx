9c729cf00fe038c7e1dc6357f483cbdc99ed34c3
Artemy-sweden <ghosteagle2020@mail.ru>
2024-10-24 14:48:27 +0300

    feat: firebase installation vol. 1

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+  // useEffect(() => {
+  //   const initializeFirebase = async () => {
+  //     console.log('firebase initialized');
+
+  //     await firestore.
+  //   }
+
+  // }, [])
   return <RootNavigator />;
 };
 
